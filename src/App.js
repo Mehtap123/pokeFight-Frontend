dd44e48ba482e2364273533aac26fd49722f39b6
Mehtap <kayhanmehtap10@gmail.com>
2022-05-25 20:55:42 +0200

    Create Routes and Pokemon-Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Landingpage from './components/Landingpage';
+import Pokemon from './components/Pokemon';
+
+
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -7,11 +13,26 @@ function App() {
     fetch("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json")
       .then((res) => res.json())
       .then((data)=> setData(data))
-      .then((data) => console.log(data))
       .catch((error) => console.log(error));
   }, []);
 
-  return ;
+  const pokemon = data;
+  console.log(pokemon);
+  console.log(data);
+  return (
+
+    <>
+    {pokemon?(
+    <Routes>
+      <Route path="/" element={<Landingpage/>}/>
+      <Route path="/pokemon" element={<Pokemon data={pokemon}/>}/>
+    </Routes>
+    ) : (
+      "Loading...."
+    )
+}
+    </>
+  )
 }
 
 export default App;
