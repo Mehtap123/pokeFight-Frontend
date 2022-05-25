31c75b724de0af571e96e7172f07155a98b4b634
Christian Scholtysik <christianscholtysik@gmail.com>
2022-05-25 20:46:03 +0200

    change landingpage, add pokemon-font

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,11 +1,24 @@
-import LOGO_Landingpage from "../img/LOGO_Landingpage.png";
+import logo from "../logo.svg";
+import pokemon from "../img/Pokemon-Logo-Schrift.png";
+import "../index.css";
 
 const Landingpage = () => {
   return (
     <div>
-      <button type="button">
-        <img src={LOGO_Landingpage} alt="Bild" height={455} width={455} />
-      </button>
+      <div className="pokemon">
+        <img src={pokemon} alt="pokemon" width={955} />
+      </div>
+      <div className="ball">
+        <button type="button">
+          <img
+            src={logo}
+            alt="Bild"
+            height={555}
+            width={555}
+            className="App-logo"
+          />
+        </button>
+      </div>
     </div>
   );
 };
