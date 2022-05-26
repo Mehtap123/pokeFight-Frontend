6bb459524801bb487873a2526ebe041436ef4e67
Christian Scholtysik <christianscholtysik@gmail.com>
2022-05-26 11:49:52 +0200

    change logo button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Landingpage from "./components/Landingpage";
+import Pokemon from "./components/Pokemon";
+
 function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json")
+    fetch(
+      "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json"
+    )
       .then((res) => res.json())
-      .then((data)=> setData(data))
-      .then((data) => console.log(data))
+      .then((data) => setData(data))
       .catch((error) => console.log(error));
   }, []);
 
-  return ;
+  const pokemon = data;
+
+  console.log(pokemon);
+  console.log(data);
+  return (
+    <>
+      <h2>hello</h2>
+      {pokemon ? (
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/pokemon" element={<Pokemon data={pokemon} />} />
+        </Routes>
+      ) : (
+        "Loading...."
+      )}
+    </>
+  );
 }
 
 export default App;
