20356654afe456146f44238ad4d23cbc00fd0dec
Ivana <rubicode01@gmail.com>
2022-05-31 18:30:56 +0200

    create responsive images

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -8,35 +8,31 @@ const Pokemon = ({ data, pictures }) => {
   console.log(pictures);
   return (
     <>
-      <div className="pokemonschrift-wrap">
-        <div className="pokemonschrift">
-          <img src={pokemon} alt="Bild" height={225} />
-        </div>
-        <SearchBar />
+      <div className="pokemonschrift-wrap ">
+        <Link to="/">
+          <img src={pokemon} alt="Bild" className="pokemonschrift" />
+        </Link>
+      </div>
+      <SearchBar />
 
-        {data.map((item, index) => (
-          <div
-            className="card poke-card"
-            style={{ width: "18rem" }}
-            key={index}
-          >
-            <div className="card-body">
-              <h4 className="card-title poke-title">{item.name.english}</h4>
-              <div className="avatar">
-                <img src={pikachu} alt="pikachu" className="fluid" width={50} />
-              </div>
-              <div className="btn-poke">
-                <a href="https://www.google.de" className="btn  buttonstyle ">
-                  Select
-                </a>
-                <Link to={`${item.id}`} className="btn buttonstyle ">
-                  Info
-                </Link>
-              </div>
+      {data.map((item, index) => (
+        <div className="card poke-card" style={{ width: "18rem" }} key={index}>
+          <div className="card-body">
+            <h4 className="card-title poke-title">{item.name.english}</h4>
+            <div className="avatar">
+              <img src={pikachu} alt="pikachu" className="fluid" width={50} />
+            </div>
+            <div className="btn-poke">
+              <a href="https://www.google.de" className="btn  buttonstyle ">
+                Select
+              </a>
+              <Link to={`${item.id}`} className="btn buttonstyle ">
+                Info
+              </Link>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </>
   );
 };
